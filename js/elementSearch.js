4f05075d2bdb814252d5a00cdc1068e0ec202e6a
Enrico Benini <benkio89@gmail.com>
2023-12-26 14:24:07 +0100

    Century trilogy + improved search inside dropdown hide/show

--- js/elementSearch.js
+++ js/elementSearch.js
@@ -1,9 +1,19 @@
   function elementSearch(listId) {
       var filter = $("#elementSearch").val().toLowerCase();
-      $("#"+ listId +" a").each((index, elem) => {
-          if (filter === "" || $(elem).text().toLowerCase().includes(filter)) {
+      var testCondition = elem => filter === "" || $(elem).text().toLowerCase().includes(filter)
+      $("#" + listId + " a").each((index, elem) => {
+          var dropdownLinks = $(elem).parents().filter("div.dropdown");
+          if (testCondition(elem)) {
+              $(dropdownLinks).show();
               $(elem).show();
           } else {
+              var childrenMatch = false
+              var dropdownHide = $(dropdownLinks).children().find("a").each((index, celem) => {
+                  childrenMatch = childrenMatch || testCondition(celem);
+              });
+              if (!childrenMatch) {
+                  $(dropdownLinks).hide();
+              }
               $(elem).hide();
           }
       });
