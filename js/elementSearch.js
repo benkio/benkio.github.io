1e90302b1815a8801ea08ec0bb963ef0d920b99b
Enrico Benini <benkio89@gmail.com>
2025-07-13 20:01:45 +0100

    30_FoldCSV - ✓ search with dropdowns

--- js/elementSearch.js
+++ js/elementSearch.js
@@ -1,11 +1,17 @@
 function elementSearch(type) {
     var filter = $("#elementSearch").val().toLowerCase();
-    var testCondition = elem => filter === "" || $(elem).text().toLowerCase().includes(filter);
+    var testCondition = elem => filter === "" || $(elem).text().toLowerCase().indexOf(filter) >= 0;
     $("." + type).each((index, elem) => {
-        if (testCondition(elem)) {
-            $(elem).show();
+        var elemToChange = null;
+        if ($(elem).is("a")) {
+            elemToChange = $(elem).closest(".dropdown");
+        } else elemToChange = elem;
+        if (testCondition(elemToChange)) {
+            $(elemToChange).show();
+            $(elemToChange).children("button").show();
         } else {
-            $(elem).hide();
+            $(elemToChange).hide();
+            $(elemToChange).children("button").hide();
         }
     });
 };
