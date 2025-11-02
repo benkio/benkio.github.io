fa252db7ab1cf8eddfde6a04728007dca4e3e534
Enrico Benini <benkio89@gmail.com>
2025-11-02 15:53:21 +0000

    🔧 search. condition on class, not element type

--- js/elementSearch.js
+++ js/elementSearch.js
@@ -1,17 +1,18 @@
 function elementSearch(type, withDropdown) {
-    var filter = $("#elementSearch").val().toLowerCase();
-    var testCondition = elem => filter === "" || $(elem).text().toLowerCase().indexOf(filter) >= 0;
-    $("." + type).each((index, elem) => {
-        var elemToChange = null;
-        if ($(elem).is("a") && withDropdown) {
-            elemToChange = $(elem).closest(".dropdown");
-        } else elemToChange = elem;
-        if (testCondition(elemToChange)) {
-            $(elemToChange).show();
-            $(elemToChange).children("button").show();
-        } else {
-            $(elemToChange).hide();
-            $(elemToChange).children("button").hide();
-        }
-    });
-};
+  var filter = $("#elementSearch").val().toLowerCase();
+  var testCondition = (elem) =>
+    filter === "" || $(elem).text().toLowerCase().indexOf(filter) >= 0;
+  $("." + type).each((index, elem) => {
+    var elemToChange = null;
+    if ($(elem).hasClass("dropdown-item") && withDropdown) {
+      elemToChange = $(elem).closest(".dropdown");
+    } else elemToChange = elem;
+    if (testCondition(elemToChange)) {
+      $(elemToChange).show();
+      $(elemToChange).children("button").show();
+    } else {
+      $(elemToChange).hide();
+      $(elemToChange).children("button").hide();
+    }
+  });
+}
